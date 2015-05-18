524caa65f6f4052ee7e52a03529cc98c01fd74d4
jdejoode <jdj@email.com>
2015-05-18 09:42:53 +0100

    Highlight things to do and change in clusters.py

--- clic/clusters.py
+++ clic/clusters.py
@@ -1,18 +1,21 @@
 import os
 import operator
 
+from cheshire3.baseObjects import Session
 from cheshire3.document import StringDocument
 from cheshire3.internal import cheshire3Root
 from cheshire3.server import SimpleServer
-from cheshire3.baseObjects import Session
 
+# FIXME make relative
 cheshirePath = os.path.join('HOME', '/home/cheshire')
 
 class Clusters(object):
 
     def __init__(self):
+        # TODO compare with concordance init
         self.session = Session()
         self.session.database = 'db_dickens'
+        # TODO read this config
         self.serv = SimpleServer(self.session,
                             os.path.join(cheshire3Root, 'configs', 'serverConfig.xml')
                             )
@@ -23,6 +26,7 @@ class Clusters(object):
         self.logger = self.db.get_object(self.session, 'clusterLogger') ## added to dbs/dickens/config.xml
 
     def list_clusters(self, idxName, Materials):
+        # TODO activate logging
         #self.logger.log(10, 'CREATING CLUSTERS FOR RS: {0} in {1}'.format(idxName, Materials))
         session = self.session
         db = self.db
@@ -39,10 +43,12 @@ class Clusters(object):
                                        ' or '.join(clauses)
                                        )
         print query
+        # TODO is db.search different from query we used before?
         results = db.search(session, query)
 
         idx = db.get_object(session, idxName)
         facets = idx.facets(session, results)
+        # FIXME do not use built_in names
         dict = {}
         for x in facets:
             dict[x[0]] = x[1][2]
@@ -58,4 +64,5 @@ class Clusters(object):
         if len(cluster_list) <= 5000:
             return cluster_list
         else:
+            # TODO make this a round number
             return cluster_list[0:4999]
