02d81c105caa449757a55ae3c93a484796c3503f
jdejoode <jdj@email.com>
2015-05-22 09:21:03 +0100

    Local settings

--- settings/local_settings.py
+++ settings/local_settings.py
@@ -1,11 +1,11 @@
-# This file contains the project wide settings for local dev. 
-# It should be adapted to suit each local setup. 
+# This file contains the project wide settings for local dev.
+# It should be adapted to suit each local setup.
 
 ### For run.py
 
 # Use the absolute path to the directory that stores the data.
 # This can differ per deployment
-# DATA_DIRECTORY = ""
+DATA_DIRECTORY = "/home/jdejoode/projects/clic/clic/dbs/dickens/data/"
 
 
 ### For the web app itself
